e4a0e8ee8b284a2133318a23befaa93015332df7
SebastianXXI05 <kaynandredblue@gmail.com>
2023-06-01 20:10:26 -0500

    [frontend] add style to not found page

--- frontend/src/Pages/NotFound.jsx
+++ frontend/src/Pages/NotFound.jsx
@@ -1,13 +1,11 @@
-import { useRouteError, Link } from 'react-router-dom'
+import { Link } from "react-router-dom"
 
 export default () => {
-  const error = useRouteError()
-
   return (
-    <div>
-      <h1>{error.status}</h1>
-      <p>{error.statusText}</p>
-      <Link to='/'>Back Home</Link>
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <h1 className="text-3xl text-gray-600">404</h1>
+      <p className="text-4xl mb-4 font-bold">Not Found</p>
+      <Link className="text-sky-600 underline hover:text-sky-800" to='/'>Back Home</Link>
     </div>
   )
 }

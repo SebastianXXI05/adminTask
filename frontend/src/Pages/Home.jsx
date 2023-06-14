e4cad4ab9d50f47a21db7696e15abdf297937979
SebastianXXI05 <kaynandredblue@gmail.com>
2023-06-14 15:21:01 -0500

    [frontend] add search funcionality to home page

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,12 +1,33 @@
-import { useLoaderData, Link, useLocation } from 'react-router-dom'
+import { useLoaderData, Link, useLocation, json } from 'react-router-dom'
 import Task from '../components/Task'
 import { useState, useEffect } from 'react'
 
 export default function Home() {
-  const { tasks } = useLoaderData()
+  const { fetchTasks } = useLoaderData()
   const location = useLocation()
   const message = location.state?.message
   const [ showMessage, setShowMessage ] = useState(false)
+  const [ search, setSearch ] = useState('')
+  const [ tasks, setTasks ] = useState(fetchTasks)
+
+  function handleSearch(title) {
+    setSearch(title)
+
+    async function searchByTitle(url) {
+      const res = await fetch(url)
+      const data = await res.json()
+      setTasks(data)
+    }
+
+    if (title.length === 0) {
+      const url = import.meta.env.VITE_API
+      searchByTitle(url)
+    }
+    else {
+      const url = `${import.meta.env.VITE_API}search?title=${title}`
+      searchByTitle(url)
+    }
+  }
 
   useEffect(() => {
     if (message !== undefined) {
@@ -39,16 +60,28 @@ export default function Home() {
         </Link>
       </div>
       <main className='border-2 border-gray-400 h-96 rounded p-4'>
-        <div className='flex justify-center border-b border-gray-400 pb-2 mb-4'>
-          <input 
-          className="border border-gray-400 rounded p-1 focus-visible:
-          outline-none focus-visible:border-sky-500 pr-9" 
-
-          type="text" 
-          placeholder="Search by title" />
-          <span>
-            <img src="x-lg.svg" alt="x image" />
-          </span>
+        <div 
+          className='flex justify-center border-b border-gray-400 pb-2 mb-4'
+        >
+          <div 
+            className="w-fit flex relative"
+          >
+            <input 
+              className="border border-gray-400 rounded p-1 focus-visible:
+              outline-none focus-visible:border-sky-500 pr-9" 
+              type="text" 
+              placeholder="Search by title" 
+              value={search}
+              onChange={(e) => handleSearch(e.target.value)}
+            />
+            <button 
+              className='absolute right-2.5 top-1.5'
+              hidden={search.length !== 0 ? false : true}
+              onClick={() => handleSearch('')}
+            >
+              <img className='w-5 h-5' src="x-lg.svg" alt="x image" />
+            </button>
+          </div>
         </div>
         <section className='overflow-y-auto h-72'>
           {
@@ -69,7 +102,7 @@ export default function Home() {
 export async function loaderHome() {
   const url = import.meta.env.VITE_API
   const res = await fetch(url)
-  const tasks = await res.json()
+  const fetchTasks = await res.json()
 
-  return { tasks }
+  return { fetchTasks }
 }
